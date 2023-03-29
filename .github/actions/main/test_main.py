65761a18dd1d6e01b0aa35e6091ed88263d102e0
Jayanth Nuggu <jayanthnuggu@gmail.com>
2023-03-29 22:39:36 +0530

    updated

--- .github/actions/main/test_main.py
+++ .github/actions/main/test_main.py
@@ -1,8 +1,10 @@
 import sys
+import logging
 sys.path.insert(0,'.github/actions/main')
 from main import add,main
 
 def test_main():
+    logging.info("runned")
     assert add(2,3)==5
 
 def test_add():
