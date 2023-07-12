a93d82b985d95f90e14dcc7998ca03e56b1fe8ec
Jayanth Nuggu <67133310+jayanth-999@users.noreply.github.com>
2023-07-12 16:00:12 +0530

    Update test_main.py

--- .github/actions/main/test_main.py
+++ .github/actions/main/test_main.py
@@ -1,11 +1,10 @@
 import sys
 import logging
 sys.path.insert(0,'.github/actions/main')
-from main import add,main
+from main import main
 
-def test_main():
-    logging.info("runned")
-    assert add(2,3)==5
-
-def test_add():
-    add(2,3)
+ef test_main(monkeypatch, capsys):
+    monkeypatch.setenv('range', '10')
+    sys.argv = ['sample.py', 'argument1']
+    main()
+    captured = capsys.readouterr()
