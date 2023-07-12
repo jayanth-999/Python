62ead1f6bfe0a8cf98208cfb7af92f72eb6ae3c3
Jayanth Nuggu <67133310+jayanth-999@users.noreply.github.com>
2023-07-12 16:04:33 +0530

    Update test_main.py

--- .github/actions/main/test_main.py
+++ .github/actions/main/test_main.py
@@ -3,7 +3,7 @@ import logging
 sys.path.insert(0,'.github/actions/main')
 from main import main
 
-ef test_main(monkeypatch, capsys):
+def test_main(monkeypatch, capsys):
     monkeypatch.setenv('range', '10')
     sys.argv = ['sample.py', 'argument1']
     main()
