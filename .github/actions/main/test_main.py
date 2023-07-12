3e751d04a0f2bd42b46d6cf4167c76f806ac3bcd
Jayanth Nuggu <67133310+jayanth-999@users.noreply.github.com>
2023-07-12 16:12:24 +0530

    Update test_main.py

--- .github/actions/main/test_main.py
+++ .github/actions/main/test_main.py
@@ -1,5 +1,8 @@
 import sys
 import logging
+import pytest
+from unittest.mock import patch
+
 sys.path.insert(0,'.github/actions/main')
 from main import main
 
@@ -8,3 +11,12 @@ def test_main(monkeypatch, capsys):
     sys.argv = ['sample.py', 'argument1']
     main()
     captured = capsys.readouterr()
+
+@pytest.fixture
+def mock_os_getenv():
+    with patch.object(os, 'getenv', return_value='10'):
+        yield
+
+def test_range_mocked(mock_os_getenv):
+    range = os.getenv('range')
+    assert range == '10'
