60ee22811b2365520a037d4db554cfb47657407c
Jayanth Nuggu <67133310+jayanth-999@users.noreply.github.com>
2023-07-19 21:41:01 +0530

    Update test_main.py

--- .github/actions/main/test_main.py
+++ .github/actions/main/test_main.py
@@ -7,10 +7,17 @@ from unittest.mock import patch
 sys.path.insert(0,'.github/actions/main')
 from main import main
 
+# @pytest.mark.parametrize("range_value", [10, 20, 30])  # Add the desired values for testing
+# def test_main(range_value):
+#     with monkeypatch.context() as m:
+#         m.setenv("RANGE", str(range_value))
+#         captured_output = capsys.readouterr()
+#         main()
+#         assert captured_output.out.strip() == str(range_value)
+
 @pytest.mark.parametrize("range_value", [10, 20, 30])  # Add the desired values for testing
-def test_main(range_value):
-    with monkeypatch.context() as m:
-        m.setenv("RANGE", str(range_value))
-        captured_output = capsys.readouterr()
-        main()
-        assert captured_output.out.strip() == str(range_value)
+def test_main(range_value, monkeypatch, capsys):
+    monkeypatch.setenv("RANGE", str(range_value))
+    main()
+    captured_output = capsys.readouterr()
+    assert captured_output.out.strip() == str(range_value)
